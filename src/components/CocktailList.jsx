544467d0294d6f9fe6714e3277f6945fb7ca9b72
chitgo <kostas.chitos@gmail.com>
2025-01-07 08:34:41 +0200

    cocktails list change

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -1,11 +1,16 @@
 import CocktailCard from './CocktailCard'
 
 function CocktailList({ drinks }) {
-  if (!drinks) {
+  if (!Array.isArray(drinks) || drinks.length === 0) {
     return (
-      <div className="h-screen px-12 text-center">
-        <div className="flex flex-col items-center mt-20 text-white ">
-          <h1 className="mt-4 text-5xl mainColor">Cocktail not found!</h1>
+      <div className='h-screen px-12 text-center'>
+        <div className='flex flex-col items-center mt-20 text-white'>
+          <h1 className='mt-4 text-5xl mainColor'>
+            Find Your Favorite Cocktails!
+          </h1>
+          <p className='mt-4 text-xl'>
+            Use the search bar above to explore delicious cocktails.
+          </p>
         </div>
       </div>
     )
@@ -23,11 +28,12 @@ function CocktailList({ drinks }) {
   })
 
   return (
-    <div className="grid gap-y-8 py-8 mt-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 w-[90%] max-w-7xl mx-auto ">
+    <div className='grid gap-y-8 py-8 mt-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 w-[90%] max-w-7xl mx-auto'>
       {formatedDrinks.map((item) => {
         return <CocktailCard key={item.id} {...item} />
       })}
     </div>
   )
 }
+
 export default CocktailList
